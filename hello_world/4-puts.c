864f4f17ce7106c0f643d98cff9f081047c166e8
Jagucan <jjgc.2499@gmail.com>
2022-10-08 16:17:14 -0700

    My first c - Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
-
 /* Desciption:
- * This program have a main and prints a sentence followed by a new line.
+ * This program have a main and prints a sentence 
  * using print.
  * The program return 0
  */
-
 int main (void){
-
-	puts("\"Programming is like building a multilingual puzzle/n");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
