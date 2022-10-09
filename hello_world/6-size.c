f1e83d86d3dc480306c71338f6d75380a0f05a69
Jagucan <jjgc.2499@gmail.com>
2022-10-09 11:57:19 -0700

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
-/*
- *
- *
- *
+/**
+ *main - print size of various types whit printf
+ *return 0 every time
  */
 int main(void)
 {
-	char;
-	int;
-	long int;
-	long long int;
-	float;
+char sChar;
+int sInt;
+long int sLongInt;
+long long int sLongLongInt;
+float sFloat;
 
-	printf("sixe of char: %i bite(s)\n", sizeof(char));
-	printf("sixe of int: %i bite(s)\n", sizeof(int));
-	printf("sixe of long int: %i bite(s)\n", sizeof(long int));
-	printf("sixe of long long int: %i bite(s)\n", sizeof(long long int));
-	printf("sixe of float: %i bite(s)\n", sizeof(float));
+printf("sixe of char: %i bite(s)\n", sizeof(sChar));
+printf("sixe of int: %i bite(s)\n", sizeof(sIntsInt));
+printf("sixe of long int: %i bite(s)\n", sizeof(sLongInt));
+printf("sixe of long long int: %i bite(s)\n", sizeof(sLongLongInt));
+printf("sixe of float: %i bite(s)\n", sizeof(sFloat));
+return (0);
 }
