35586c83ce073d45905e986ce2be66f5126462c4
Jagucan <jjgc.2499@gmail.com>
2022-10-09 11:43:10 -0700

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*
+ *
+ *
+ *
+ */
+int main(void)
+{
+	char;
+	int;
+	long int;
+	long long int;
+	float;
+
+	printf("sixe of char: %i bite(s)\n", sizeof(char));
+	printf("sixe of int: %i bite(s)\n", sizeof(int));
+	printf("sixe of long int: %i bite(s)\n", sizeof(long int));
+	printf("sixe of long long int: %i bite(s)\n", sizeof(long long int));
+	printf("sixe of float: %i bite(s)\n", sizeof(float));
+}
