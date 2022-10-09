2dc49078488498a34095ae430ccf75398ae9d4e4
Jagucan <jjgc.2499@gmail.com>
2022-10-08 17:03:18 -0700

    My first c - Printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - print using printf
+ *Return: 0 every time
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
