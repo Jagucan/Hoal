dc475ed38d1ad0a00faab1e691d71705b0bf5372
Jagucan <jjgc.2499@gmail.com>
2022-10-10 12:38:40 -0700

    if and else

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if (n > 0)
+	if(n > 0)
 	       printf("is positive/n", n);
-	else if (n < 0)
+	else if(n < 0)
 		printf("in negative/n", n);
-	else (n == 0)
+	else if(n == 0)
 		printf("is zero/n", n);
 	return (0);
 }
