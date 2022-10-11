d1ea6ae3f999276740637da6ad8b656ee43d5a65
Jagucan <jjgc.2499@gmail.com>
2022-10-11 08:55:30 -0700

    alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print an alphabet in reverse
+ * Return: 0 every time
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 'z'; j >= 'a'; j--)
+	{
+		putchar(j);
+	}
+		putchar('\n');
+
+return (0);
+}
