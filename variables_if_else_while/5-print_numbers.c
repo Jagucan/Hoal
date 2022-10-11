761c3a45b8c5f478329e43702d710141fee2599b
Jagucan <jjgc.2499@gmail.com>
2022-10-11 08:18:51 -0700

    munbers 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print number of base 10
+ * Return: 0 every time
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		putchar(j);
+	}
+		putchar('\n');
+return (0);
+}
