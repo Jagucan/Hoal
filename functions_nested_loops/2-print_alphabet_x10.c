5cd1bc43b97facae001ffc31ad9bd20a966909da
Jagucan <jjgc.2499@gmail.com>
2022-10-11 15:29:44 -0700

    print alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print an alphabet in lowercase
+ * Return: 0 every time
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+		_putchar('\n');
+
+return (0);
+}
