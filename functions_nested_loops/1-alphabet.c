d00aa4321c177425fe7ec2f6c99f0ff23c43568e
Jagucan <jjgc.2499@gmail.com>
2022-10-11 15:45:58 -0700

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,10 +8,9 @@ void print_alphabet(void)
 {
 	char letters;
 
-	for (letters = '97'; letters < '123'; letters++)
+	if (letters = 'a'; letters <= 'z'; letters++)
 	{
 		_putchar(letters);
 	}
 		_putchar('\n');
-
 }
