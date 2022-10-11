1acf8eabed1cbbd25255c9c35a39ccbc75cd6a9e
Jagucan <jjgc.2499@gmail.com>
2022-10-10 17:16:13 -0700

    Alphabet whit putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main contain putchar not printf for prints the alphabet
+ * main contain putchar for prints the alphabet
  * return 0 every time
  */
 
 int main(void)
 {
 	char j;
-	j='a';
+	j = 'a';
 
-	while (j <= 'z')
+	while (j < = 'z')
 	{
 		putchar(j);
 		j++;
