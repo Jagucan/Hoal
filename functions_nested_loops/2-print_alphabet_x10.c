b54b5671a3d56fd5f92cc0a844480a1cba9118c9
Jagucan <jjgc.2499@gmail.com>
2022-10-11 16:14:08 -0700

    Alphabet x 10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,13 @@ void print_alphabet_x10(void)
 	char alphabet = 'a';
 	int num = '0';
 
-	while (num <= 10; num++)
+	while (num <= 10)
 	{
 		while (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{	
 			_putchar(alphabet);
 		}
 			_putchar('\n');
+			num++;
 	}
 }
