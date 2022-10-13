7153db4b57c42b706092ef53ac2df00292afe96e
Jagucan <jjgc.2499@gmail.com>
2022-10-12 18:29:46 -0700

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,18 +13,20 @@ void jack_bauer(void)
 
 	for (h1 = '0'; h1 <= '2'; h1++)
 	{
-		for (h2 = '0'; h2 <= '3'; h2++)
+		for (h2 = '0'; h2 <= '9'; h2++)
 		{
 			for (m1 = '0'; m1 <= '5'; m1++)
 			{
 				for (m2 = '0'; m2 <= '9'; m2++)
 				{
-					_putchar(h1);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m1);
-					_putchar(m2);
-					_putchar('\n');
+					if (h1 == '2' && h2 >= '4')
+						break;
+						_putchar(h1);
+						_putchar(h2);
+						_putchar(':');
+						_putchar(m1);
+						_putchar(m2);
+						_putchar('\n');
 				}
 			}
 		}
