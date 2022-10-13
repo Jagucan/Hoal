09f8d89f152aecbaab43952e66b9e3e5dab23aa3
Jagucan <jjgc.2499@gmail.com>
2022-10-12 19:02:37 -0700

    a+b

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,6 +3,8 @@
 /**
  * add - Adds two interger numbers and return the result.
  * Return: an result.
+ * @n1: is an interger number.
+ * @n2: is other interger number.
  */
 
 int add(int n1, int n2)
