18ebac98957ea80e33d9fb930a4952fb7b475aef
Jagucan <jjgc.2499@gmail.com>
2022-10-12 18:16:42 -0700

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,22 +1,35 @@
 #include "main.h"
 
 /**
- * jack_bauer - Print every minute of the day.
+ * jack_bauer - Print every minute of the day
+ * Return: 0 every time.
  */
 
 void jack_bauer(void)
 {
-	int h;
-	int m;
+	int h1;
+	int h2;
+	int m1;
+	int m2;
 
-	for (h = '0'; h < 24; h++)
+	for (h1 = '0'; h1 <= '2'; h1++)
 	{
-		for (m = '0'; m < 59; m++)
+		for (h2 = '0'; h2 <= '3'; h2++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '0');
+			for (m1 = '0'; m1 <= '5'; m1++)
+			{
+				for (m2 = '0'; m2 <= '9'; m2++)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+				}
+			}
 		}
-			_putchar('\n');
 	}
+
+	return (0);
 }
