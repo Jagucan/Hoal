baab135769e3b6a21dd1bb0396a5cbd691ed585d
Jagucan <jjgc.2499@gmail.com>
2022-10-15 16:09:51 -0700

    square

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle _ prints a triangle, followed by a new line.
+ * size: Is the size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+
+	int row;
+	int col;
+
+	if (size > 0)
+	{
+
+		for (row = 1; row <= size; row++)
+		{
+
+			for (col = size - row; col > 0; col--)
+				_putchar(' ');
+
+			for (col = 0; col < row; col++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
+
+	else
+	{
+		_putchar('\n');
+	}
+
+}
