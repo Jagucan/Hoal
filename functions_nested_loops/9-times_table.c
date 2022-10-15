b2705ca5cc4cfa49e11fd3df9a243a96c7cf7330
Jagucan <jjgc.2499@gmail.com>
2022-10-15 11:47:42 -0700

    table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,31 +10,29 @@ void times_table(void)
 	int row = 0;
 	int col = 0;
 	int num;
-
-	while(row <= 9)
+	
+	for (row = 0; row <= 9; row++)
 	{
-		row++;
-
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
 
 		while (col <= 9)
 		{
-			num = (col * row);
 			col++;
+			num = (col * row);
 			_putchar((num % 10) + '0');
 
 			if ((num / 10) > 0)
 			{
 				_putchar((num / 10) + '0');
 			}
-			
+
 			else 
 			{
 				_putchar(' ');
 			}
-			
+
 			if (col < 9)
 			{
 				_putchar(',');
