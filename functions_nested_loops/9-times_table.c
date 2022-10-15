3d5881cfbf3debf40aaaafac71035c85952f4de0
Jagucan <jjgc.2499@gmail.com>
2022-10-15 11:31:29 -0700

    table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void times_table(void)
+{
+
+	int row = 0;
+	int col = 0;
+	int num = row * col;
+
+	while(row <= 9)
+	{
+		row++;
+
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		while (col <= 9)
+		{
+			col++;
+			_putchar((num % 10) + '0');
+
+			if ((num / 10) > 0)
+			{
+				_putchar((num / 10) + '0');
+			}
+			
+			else 
+			{
+				_putchar(' ');
+			}
+			
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}	
+				_putchar('\n');
+	}
+}
+
