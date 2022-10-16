b2fceb8feac45fcb25992e6db60e8a9af4399dda
Jagucan <jjgc.2499@gmail.com>
2022-10-16 11:56:27 -0700

    prints an integer

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: I an integer.
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+
+	if ((i / 10) > 0)
+
+		print_number(i / 10);
+
+	_putchar((i%10) + '0');
+
+}
