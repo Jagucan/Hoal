9e4d7bc8dcdb68f8671783c5a003a96d04c36912
PC - 02 <jjgc.2499@gmail.com>
2022-10-17 17:21:06 -0500

    swaps the values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Is an interger.
+ * @b: Is another interger.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int aux = *a;
+
+	*a = *b;
+	*b = aux;
+}
