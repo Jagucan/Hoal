2fd92c1af484c995f6c55f69491ee5ac3a65695f
PC - 02 <jjgc.2499@gmail.com>
2022-10-18 18:25:11 -0500

    string rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,20 +2,28 @@
 
 /**
  * print_rev - Prints a string, in reverse.
+ * @s:Is an pointer.
  */
 
 void print_rev(char *s)
 {
-	char str = *s;
+	int p = *s;
 	int rev = 0;
 
-	while (str != '\0')
+	while (*s != '\0')
 	{
-		str = *s++;
+		p = *s++;
+		rev++;
+	}
 
-		while (rev >= *s)
+	if (rev != 0)
+	{
+		p = *s--;
+		
+		while (rev > 0)
 		{
-			_putchar(str);
+			p = *s--;
+			_putchar(p);
 			rev--;
 		}
 	}
