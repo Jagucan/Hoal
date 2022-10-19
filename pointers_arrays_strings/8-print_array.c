24dd1dabae4fd936c4a7ebd2f0e04e576720452c
PC - 02 <jjgc.2499@gmail.com>
2022-10-19 17:00:39 -0500

    array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 /**
- *
+ * print_array - Prints n elements of an array of integers.
+ * @a: Is a pointer.
+ * @n: Is an interger.
  */
 
 void print_array(int *a, int n)
@@ -16,7 +18,7 @@ void print_array(int *a, int n)
 
 		else
 			printf(", %d", a[i]);
+	}
 
-	}	
-
+	printf("\n");
 }
