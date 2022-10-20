76c1fac1fd53013c5c564e84d346a1622c5622bb
PC - 02 <jjgc.2499@gmail.com>
2022-10-20 12:50:41 -0500

    C Pointers

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int j = 0;
+	int i = 0;
+
+	while (dest[j] != '\0')
+		j++;
+
+	while (src[i] != '\0') 
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 
-
+/* C - Pointers, arrays and strings. */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,5 +13,15 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int  _putchar ( char c);
+
+/* C - More pointers, arrays and strings. */
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 
 #endif
