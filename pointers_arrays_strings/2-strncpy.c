df7f031092e0e506fd930e555cbf0873c85d95dd
PC - 02 <jjgc.2499@gmail.com>
2022-10-21 18:26:26 -0500

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,8 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (a = 0; a < n && src[a] != '\0'; a++)
 		dest[a] = src[a];
-	while (a < n)
-		a++;
+	
+	for (a = 0; a < n; a++)
 		dest[a] = '\0';
 
 	return (dest);
