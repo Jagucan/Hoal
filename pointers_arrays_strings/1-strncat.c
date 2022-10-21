beecafd117af887d6667c27467bfbc94e681de8f
PC - 02 <jjgc.2499@gmail.com>
2022-10-21 18:44:56 -0500

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,16 +14,11 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 
 	while (dest[a] != '\0')
-	{
 		a++;
-	}
-
 	while (i < n && src[i] != '\0')
-	{
 		dest[a + i] = src[i];
 		i++;
 		a++;
-	}
 
 	dest[a + i] = '\0';
 	return (dest);
