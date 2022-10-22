a7d2fbd664473dd204c1269469f35292320a4408
PC - 02 <jjgc.2499@gmail.com>
2022-10-21 20:19:15 -0500

    strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -11,19 +11,17 @@ char *_strpbrk(char *s, char *accept)
 {
 	int a;
 
-	while (*s != '\0')
+	while (*s)
 	{
-		s++;
+
 		for (a = 0; accept[a]; a++)
 		{
-
 			if (*s == accept[a])
-			{
 				return (s);
-			}
 		}
-	
+
+		s++;
 	}
 
-	return (0);
+	return ('\0');
 }
