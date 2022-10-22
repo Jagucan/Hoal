cad6752770c264c1c1f74ede1571c66059114756
PC - 02 <jjgc.2499@gmail.com>
2022-10-21 22:04:05 -0500

    main

---
+++ pointers_arrays_strings/1x1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[98] = "Holberton ";
+	char s2[] = "School!\n";
+	char *p;
+
+	printf("%s\n", s1);
+	printf("%s", s2);
+	p = _strncat(s1, s2, 50);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	printf("%s\n", p);
+	return (0);
+}
