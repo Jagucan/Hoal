c901499d4cf27d9dda3e3d5e22137b89e649209e
PC - 02 <jjgc.2499@gmail.com>
2022-10-21 20:31:45 -0500

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,12 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 
 	for (a = 0; dest[a] != '\0'; a++)
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[i] = src[a];
+		for (i = 0; i < n && src[i] != '\0'; i++)
+		{
+			dest[i] = src[a];
+		}
 	}
-
-	dest[i + a] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
