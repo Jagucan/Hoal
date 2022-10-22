1330d5856e34c80c0c033edeaf2152a2a0e04ef1
PC - 02 <jjgc.2499@gmail.com>
2022-10-21 22:15:30 -0500

    main

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -1,19 +1,18 @@
+#include "main.h"
 #include <stdio.h>
 
-char *string_toupper(char *);
-
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s[] = "Hello";
-	char *p;
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
 
-	p = string_toupper(s);
-	printf("%s\n", p);
-	printf("%s\n", s);
-	return (0);
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
 }
