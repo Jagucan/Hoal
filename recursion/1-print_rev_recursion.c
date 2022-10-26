77e858e2cd865f5cb0a0b626b5d696c6291971af
Jacinto Gutierrez <113849920+Jagucan@users.noreply.github.com>
2022-10-26 17:11:44 -0500

    Update 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,15 +3,13 @@
 /**
  * _print_rev_recursion - Prints a string in reverse.
  * @s: Is a pointer.
-*/
+ */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-
 	}
-
 }
