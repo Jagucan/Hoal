7d68d1cad231261f06bb1ceab3e5e89cc91b3f2b
Jacinto Gutierrez <113849920+Jagucan@users.noreply.github.com>
2022-10-26 17:04:29 -0500

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,10 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	int a = *s;
+
+	if (a != 0)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(a);
+		_puts_recursion(a + 1);
 	}
 
 }
