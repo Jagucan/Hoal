97faa3415b728f5c154439fa09d1a91c3fe335d1
Jacinto Gutierrez <113849920+Jagucan@users.noreply.github.com>
2022-10-26 13:26:26 -0500

    C - Recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+ /**
+  * _puts_recursion - Prints a string.
+  * @s: Is a pointer.
+ */
+
+void _puts_recursion(char *s)
+{
+    if (*s)
+    {
+        /* Print content of pointer s */
+        _putchar(*s);
+        /* Change to the next character in the string*/
+        _puts_recursion(s + 1);
+    }
+    _putchar('\n');
+}
