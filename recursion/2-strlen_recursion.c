8abbca1aff99cde4e3ab69413dfab535b5ca24e2
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-10-27 14:54:47 -0500

    Strlen Rec

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: Is a pointer.
+ * Return: Returns the length of a string.
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
