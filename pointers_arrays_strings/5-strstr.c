32b3870862ea87bd688cd81f6e8265bc9c8c0871
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-10-28 19:12:15 -0500

    commit

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: Is a pointer.
+ * @needle: Is a pointer.
+ * Return: Return a pointer.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+
+	if (*needle == '\0')
+		return (haystack);
+
+	while (*haystack)
+	{
+		a = 0;
+
+		if (haystack[a] == needle[a])
+		{
+			do {
+					if (needle[a + 1] == '\0')
+						return (haystack);
+						a++;
+				}
+
+			while (haystack[a] == needle[a]);
+		}
+
+			haystack++;
+	}
+
+	return ('\0');
+
+}
