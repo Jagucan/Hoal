802d0df33620f293a05783c3373e6f9304e28c1e
Jagucan <jjgc.2499@gmail.com>
2022-11-02 10:59:33 -0500

    alloc grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+{
+	int **gr;
+	int a;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	gr = malloc(height * sizeof(int *));
+	if (gr == 0)
+	{
+		free (gr);
+		return (NULL);
+	}
+
+	for (a = 0; a < height; a++)
+	{
+
+		gr[a] = malloc(width * sizeof(int *));
+		if (gr[a] == 0)
+		{
+			for (a = a -1; a >= 0; a++)
+			{
+				free (gr[a]);
+				return (NULL);
+			}
+		}
+	}
+
+	return (gr);
+}
