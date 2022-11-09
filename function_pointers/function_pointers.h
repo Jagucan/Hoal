7f67a3ffc63cc15a3a9f24d7a9b2f7a63abe1fca
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-09 10:13:09 -0500

    commit

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 #include <stdio.h>
 #include <stddef.h>
 
@@ -10,3 +13,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
+#endif
