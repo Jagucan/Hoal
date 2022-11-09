36af94a779ad16997e38e8028dbcc6833f122045
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-09 16:31:45 -0500

    C - Structures, typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,10 +11,7 @@ struct dog
     char *owner;
 };
 
-typedef struct dog
-{
-    dog_t;
-};
+typedef struct dog dog_t;
 
 /* Prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
