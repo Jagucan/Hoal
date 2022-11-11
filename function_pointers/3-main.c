dbb9d32320f868142b524fede440d39def1d206d
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-11 11:52:59 -0500

    Calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2;
 	char *op;
 
 	if (argc != 4)
@@ -22,19 +22,18 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	op = argv[2];
 
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' || *op == '%') && (num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = get_op_func(op)(num1, num2);
-	printf("%d\n", result);
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
