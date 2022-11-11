fbbe171c5233a8874991a4794518d2b322cf35e0
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-11 12:34:27 -0500

    Calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,8 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = 0, num2 = 0, result;
-	char *op;
+	int num1 = 0, num2 = 0;
+	char *op == NULL;
 
 	if (argc != 4)
 	{
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	result = get_op_func(op)(num1, num2);
-	printf("%d\n", result);
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
