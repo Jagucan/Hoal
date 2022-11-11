466999f4e296980178b074444582eda0bba09213
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-11 10:15:02 -0500

    executes a function

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,8 +1,19 @@
-#include "funcion_pointers"
+#include "function_pointers.h"
 
 /**
  * 
  * 
 */
 
-void array_iterator(int *array, size_t size, void (*action)(int)) 3
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int num;
+
+	if (array == NULL || action __ NULL)
+		return (1);
+
+	for (num = 0; num < size; num++)
+	{
+		action(array[num]);
+	}
+}
