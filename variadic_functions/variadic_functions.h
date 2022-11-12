219ac07aed23c16efce06b032b0825e69ffb7960
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-11 20:55:18 -0500

    prints anything.

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,6 +6,12 @@
 #include <stddef.h>
 #include <stdarg.h>
 
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_a;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
