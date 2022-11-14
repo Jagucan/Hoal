ad6346292b66cd8a232a0a1438a04d22de04c37f
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-14 15:07:04 -0500

    Print anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,69 +1,51 @@
 #include "variadic_functions.h"
 
 /**
- * 
- * 
- * 
+ * print_all - Prints anything.
+ * format: Is  list of types of arguments.
 */
-
-void print_char(va_list c)
-{
-	printf("%d", va_arg(c, int));
-}
-
-void print_int(va_list i)
-{
-	printf("%d", va_arg(i, int));
-}
-
-void print_float(va_list f)
-{
-	printf("%f", va_arg(f, double));
-}
-
-void print_str(va_list s)
-{
-	char *str;
-
-	str = va_arg(s, char *);
-
-	if (!str)
-		str = "(nil)";
-	printf("%s", str);
-}
 
 void print_all(const char * const format, ...)
 {
-	print_a types_args[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_str},
-		{NULL, NULL}
-	};
-
 	va_list print_args;
-	int a = 0, i = 0;
-	char *separator = NULL;
+
+	int a = 0;
+	char *str;
+	char *separator = " ";
 
 	va_start(print_args, format);
 
-	a = 0;
-	while (format && format[a])
+	while (format != NULL && format[a] != NULL)
 	{
-		i = 0;
-		while (types_args[i].t != NULL)
+		switch (print_args[a])
 		{
-			if (*(types_args[i].t) == format[a])
-			{
-				printf("%s", separator);
-				types_args[i].f(print_args);
-				separator = ", ";
+			case 'c':
+				printf("%s%d", separator, va_arg(print_args, int));
 				break;
-			}
-			i++;
+
+			case 'i':
+				printf("%s%d", separator, va_arg(print_args, int));
+				break;
+
+			case 'f':
+				printf("%s%f", separator, va_arg(print_args, double));
+				break;
+			
+			case 's':
+				str = va_arg(print_args, char *);
+
+				if (str == NULL)
+					str = "(nil)";
+
+				printf("%s%s", separator, str);
+
+			default:
+				a++;
+				continue;
 		}
+		separator = COM;
 		a++;
+
 	}
 	printf("\n");
 	va_end(print_args);
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,16 +1,11 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#define COM ", "
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdarg.h>
-
-typedef struct print
-{
-	char *t;
-	void (*f)(va_list);
-} print_a;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
