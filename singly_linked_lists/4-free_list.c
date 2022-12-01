98ee91d95bc4d6eccc85fab438e78233f978fb63
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-01 16:09:31 -0500

    Free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list.
+ * @head: Pointer to list.
+*/
+
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		free(head->str);
+		head = aux;
+	}
+	
+}
