49f456c834b5dd3ce2d1a34c761c822eded1ea26
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-02 17:02:51 -0500

    converts a binary

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,19 +12,19 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int res = 1, conv_int = 0;
 
 	if (!b)
-		return(0);
+		return (0);
 
 	max_idx = strlen(b) - 1;
 
 	while (max_idx >= 0)
 	{
 		if (b[max_idx] != '0' && b[max_idx] != '1')
-			return(0);
+			return (0);
 
 		conv_int += res * (b[max_idx] - '0');
 		res <<= 1;
 		max_idx--;
 	}
 
-	return(conv_int);
+	return (conv_int);
 }
