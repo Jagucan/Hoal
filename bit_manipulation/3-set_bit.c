f00cfb14f3e24de891f4250a215d05ab15b733ac
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-02 22:08:52 -0500

    Set Bit

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit - Sets the value of a bit to 1 at a given index.
  * @n: Is an unsigned int.
  * @index: Is an index of the bit.
- * Returns: 1 if it worked, or -1 if an error occurred.
+ * Return: 1 if it worked, or -1 if an error occurred.
 */
 
 int set_bit(unsigned long int *n, unsigned int index)
@@ -15,7 +15,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (n == 0 && index < 64)
 		return (0);
 
-	if (index >= 64)
+	if (index >= (sizeof(int) * 8))
 		return (-1);
 
 	*n = *n | (1 << index);
