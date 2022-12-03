3503e4346ef1642add76652f383e14cc10f97650
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-02 22:16:18 -0500

    Set Bit

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -12,9 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (!index)
 		return (0);
 
-	if (n == 0 && index < sizeof(int) * 8)
-		return (0);
-
 	if (index >= sizeof(int) * 8)
 		return (-1);
 
