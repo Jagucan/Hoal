d87699ca5fbf357aa32c396644e5f1aee7aad2d9
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-04 14:21:17 -0500

    flip bits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int t_bit, num, b_idx;
 	unsigned int bits = 0;
 	
-	for (t_bit = 0; t_bit <= 32; t_bit++)
+	for (t_bit = 0; t_bit <= 32 - 1; t_bit++)
 	{
 		num = (n >> t_bit)& 1;
 		b_idx = (m >> t_bit)& 1;
