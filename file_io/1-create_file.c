eb438dfbf4bcf482541518824bbdbb361b81a4ed
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-07 17:12:28 -0500

    creates a file.

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: Name of the file to createt.
+ * @text_content: Is a string to write to the file.
+ * Return: Returns: 1 on success, -1 on failure.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w;
+	int text = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text = strlen(text_content);
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, text);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	
+	close(o);
+	return(1);
+}
