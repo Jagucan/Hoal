af6c0d9b5fc920717629283e7edbdbcc29a31361
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-07 16:35:31 -0500

    Tread lightly, she is near

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the standard output.
  * @filename: Is a file name.
  * @letters: Is a number of letter on the text.
  * Return: Returns the actual number of letters it could read and print.
@@ -9,13 +9,13 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    	ssize_t o, r, w;
-	    char *text;
+		ssize_t o, r, w;
+		char *text;
 
 	if (!filename)
 		return (0);
 
-	text = malloc(sizeof(*text) * letters);
+	text = malloc(sizeof(*text) * letters - 1);
 	if (!text)
 		return (0);
 
