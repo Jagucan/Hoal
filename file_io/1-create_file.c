55799e4f00ea33477a4d4b5c23b60a1a6aa9fa05
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-07 17:16:36 -0500

    creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,13 +19,15 @@ int create_file(const char *filename, char *text_content)
 	{
 		o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		w = write(o, text_content, text);
+
+		if (o == -1 || w == -1)
+		return (-1);
 	}
 		
 	else	
 		text = strlen(text_content);
 
-	if (o == -1 || w == -1)
-		return (-1);
+	
 	
 	close(o);
 	return(1);
