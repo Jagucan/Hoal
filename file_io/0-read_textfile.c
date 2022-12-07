bd22db2be72504d70eff345c4f93e661336f16de
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-07 16:24:20 -0500

    Tread lightly, she is near

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: Is a file name.
+ * @letters: Is a number of letter on the text.
+ * Return: Returns the actual number of letters it could read and print.
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
