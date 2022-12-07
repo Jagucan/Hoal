72834ff78610cfbd6f4d847f8096f786f160a19c
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-07 13:55:13 -0500

    Creates a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,23 +1,22 @@
 #include "hash_tables.h"
 
 /**
- * 
- * 
- * 
+ * hash_table_create - Creates a hash table.
+ * @size: Is the size of the array.
+ * Return: Returns a pointer to the newly created hash table.
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *n_tab;
 	hash_node_t **n_node;
-	unsigned long int i;
 
 	n_tab = malloc(sizeof(hash_table_t));
 	if (!n_tab)
 		return (NULL);
 
 	n_node = malloc(sizeof(hash_node_t *) * size);
-	if (!n_node)
+	if (n_node == NULL)
 	{
 		free(n_tab);
 		return (NULL);
@@ -25,9 +24,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	n_tab->size = size;
 	n_tab->array = n_node;
-
-	for (i=0; i < size; i++)
-	    n_node[i] = NULL;
 
 	return (n_tab);
 }
