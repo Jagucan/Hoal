d2e029930374b5b2601dc58bada713fea8c6d8fc
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-07 16:32:48 -0500

    Tread lightly, she is near

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,27 +9,21 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    	ssize_t open, read, write;
+    	ssize_t o, r, w;
 	    char *text;
 
 	if (!filename)
 		return (0);
 
-	text = malloc(sizeof(letters));
+	text = malloc(sizeof(*text) * letters);
 	if (!text)
 		return (0);
 
-	open = o(filename, O_RDONLY);
-	read = r(open, text, letters);
-	write = w(STDOUT_FILENO, text, read);
-
-	if (open == -1 || read == -1 || write == -1)
-	{
-		free(text);
-		return (0);
-	}
+	o = open(filename, O_RDONLY);
+	r = read(o, text, letters);
+	w = write(STDOUT_FILENO, text, r);
 
 	free(text);
-	close(open);
-	return (write);
+	close(o);
+	return (w);
 }
