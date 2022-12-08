922129779996ce9e1d98ee498b89e1e4c7a94554
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-08 14:12:08 -0500

    free list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * _free_dlist - Realease the memory allocated for a list
- *
+ * free_dlist - Realease the memory allocated for a list
  * @head: A pointer to the first node of the list to free
  */
+
 void free_dlistint(dlistint_t *head)
 {
     dlistint_t *aux;
