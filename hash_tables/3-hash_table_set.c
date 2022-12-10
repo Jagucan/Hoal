4f74a272d7d62e73390f92c6baf2bcebbf05f8db
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-09 19:56:50 -0500

    Adds an element

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -38,7 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] && strcmp(ht->array[index]->key, key_dup) != 0)
 		node->next = ht->array[index];
 
-	if (ht->array[index] = NULL)
+	if (ht->array[index])
 	{
 		free(ht->array[index]->key);
 		free(ht->array[index]->value);
