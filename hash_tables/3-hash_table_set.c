584d0d1ea921b7a7d0389060c5ba0a649a470703
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-09 20:00:37 -0500

    Adds an element

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -42,7 +42,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(ht->array[index]->key);
 		free(ht->array[index]->value);
-		free(ht->array[index]);
 	}
 
 	ht->array[index] = node;
