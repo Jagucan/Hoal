9ce513cf82fd7a748b0acca9543aa2b8dd20b363
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-12-11 11:53:30 -0500

    insert node

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node, *cop_head = *head, *aux = *head;
 	unsigned int a, len = 0;
 
-	node = malloc(sizeof(listint_t));
+	node = (listint_t *)malloc(sizeof(listint_t));
 	if (!node)
 		return (NULL);
 
