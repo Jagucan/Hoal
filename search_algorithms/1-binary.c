95c9d35872e7edeadfc8c7c90d72cc13df87fd60
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-11 11:27:55 -0500

    Binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -25,7 +25,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		printf("Searching in array: ");
 		for (aux = l_limit; aux <= u_limit; aux++)
-			printf("%d ", array[aux]);
+			printf("%d, ", array[aux]);
 
 		printf("\n");
 		mid = (l_limit + u_limit) / 2;
